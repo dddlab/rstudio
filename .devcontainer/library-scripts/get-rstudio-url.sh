8bb78dc0b1675756d00f4354ff97e1179a69607f
Sang-Yun Oh <syoh@ucsb.edu>
2022-06-23 17:53:50 +0000

    mark

--- .devcontainer/library-scripts/get-rstudio-url.sh
+++ .devcontainer/library-scripts/get-rstudio-url.sh
@@ -1,3 +1,5 @@
 #/bin/bash
 
-jupyter lab list | grep http | sed 's/codespaces-[^:]*/127.0.0.1/;s|?token|rstudio/?token|' | cut -d' ' -f1
+TOKEN=$(jupyter lab list | grep -o 'token=[[:alnum:]]*')
+
+echo "http://127.0.0.1:8888/?$TOKEN"
